4e0ae72666438df8ea72a1cfa746ebf8989b0207
edsuelila <eknsuelila3@gmail.com>
2025-10-09 16:20:54 -0400

    Registration Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Home from "./home/home"
 import Navbar from "./navbar/navbar"
+import RegistrationPage from "./registration/RegistrationPage";
 
 const App = ()=>{
-    return <main>
-        <Navbar></Navbar>
-        <Home></Home>
-        
-     </main>
+        return(
+        <>
+            <main>
+                <Navbar></Navbar>
+                <Home></Home>
+            </main>
+            <div>
+                <h1>Climate Chronicler</h1>
+                <RegistrationPage />
+            </div>
+        </>
+    )
+     
 }
 
 export default App
