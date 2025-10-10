e1b5021fcc353667a27e8caa7895e11a310c587f
diginthomas <diginthomas12@gmail.com>
2025-10-10 13:29:02 -0400

    add css

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,7 +1,9 @@
 
 const Home = ()=>{
-    return <div>
-        Home page
+    return   <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+       Home
+      </h1>
     </div>
 }
 
--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
+    <nav className="navbar-main" >
       <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
       <Link to="/login">Login</Link>
     </nav>
